6342dc9dc0e049e7af964a5e0ffd2a53831988ce
Igorextreme <168686722+Igorextreme@users.noreply.github.com>
2024-05-08 16:56:59 -0300

    Update server.js

--- chat/backend/src/server.js
+++ chat/backend/src/server.js
@@ -1,43 +1,16 @@
-const { WebSocketServer } = require("ws");
-const dotenv = require("dotenv");
+const { WebSocketServer } = require("ws")
+const dotenv = require("dotenv")
 
-dotenv.config();
+dotenv.config()
 
-const wss = new WebSocketServer({ port: process.env.PORT || 8080 });
-
-let onlineUsers = 0; // Adicionado para contar os usuários online
+const wss = new WebSocketServer({ port: process.env.PORT || 8080 })
 
 wss.on("connection", (ws) => {
-    // Incrementa o contador de usuários online quando um novo cliente se conecta
-    onlineUsers++;
-    console.log("Novo cliente conectado. Total de usuários online:", onlineUsers);
-
-    // Envia o número atual de usuários online para todos os clientes
-    wss.clients.forEach((client) => {
-        client.send(JSON.stringify({ type: 'onlineUsers', count: onlineUsers }));
-    });
-
-    ws.on("error", (error) => {
-        console.error("Erro de conexão:", error);
-    });
-
-    ws.on("close", () => {
-        // Decrementa o contador de usuários online quando um cliente se desconecta
-        onlineUsers--;
-        console.log("Cliente desconectado. Total de usuários online:", onlineUsers);
-
-        // Envia o número atual de usuários online para todos os clientes
-        wss.clients.forEach((client) => {
-            client.send(JSON.stringify({ type: 'onlineUsers', count: onlineUsers }));
-        });
-    });
+    ws.on("error", console.error)
 
     ws.on("message", (data) => {
-        // Reenvia a mensagem recebida para todos os clientes
-        wss.clients.forEach((client) => {
-            if (client !== ws && client.readyState === WebSocket.OPEN) {
-                client.send(data);
-            }
-        });
-    });
-});
+        wss.clients.forEach((client) => client.send(data.toString()))
+    })
+
+    console.log("client connected")
+})
